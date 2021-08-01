bafdfff38012ec4e176194ef8e7ddfa92bebf1e2
Simanga <simacoder@hotmail.com>
2021-08-01 04:02:16 +0200

    SebenzaApp updated

--- app/src/main/java/com/example/basicappsimanga/MainActivity.kt
+++ app/src/main/java/com/example/basicappsimanga/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.basicappsimanga
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         val actionBar = supportActionBar
         actionBar!!.title ="Home Page"
@@ -21,5 +26,17 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+
+        fun getSet(view: View){
+            val editTxt = findViewById<EditText>(R.id.streetAddresInput)
+            val msg = editTxt.text.toString()
+            val texView =findViewById<TextView>(R.id.bottomTextView).apply{
+                text = msg
+            }
+
+        }
+
     }
+
+    fun getSet(view: View) {}
 }
--- app/src/main/java/com/example/basicappsimanga/SplashScreenActivity.kt
+++ app/src/main/java/com/example/basicappsimanga/SplashScreenActivity.kt
@@ -1,13 +1,33 @@
 package com.example.basicappsimanga
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        /*val  topAnimation = AnimationUtils.loadAnimation(this,R.anim.top_animation)
+        val  middleAnimation = AnimationUtils.loadAnimation(this,R.anim.middle_animation)
+        val  bottomAnimation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation)
+
+        topTextView.startAnimation(topAnimation)
+        middleTextView.startAnimation(middleAnimation)
+        bottomTextView.startAnimation(bottomAnimation)
+        val mainActivityTimeOut = 4000
+        val homeIntent = Intent (this@SplashScreenActivity, MainActivity::class.java)
+
+        Handler().postDelayed({
+            startActivity(homeIntent)
+            finish()
+        }, mainActivityTimeOut.toLong())*/
 
     }
+
+    fun getSet(view: View) {}
 }
